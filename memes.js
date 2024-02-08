4486144986a4911a22d02b999785cb9202bf7287
icarante <icarus.wedekind23@student.cart.org>
2024-02-08 13:48:48 -0800

    idk

---
+++ memes.js
@@ -0,0 +1,12 @@
+var cactuar = document.getElementById('cactuar');
+var axel = document.getElementById('axel');
+var deuce = document.getElementById('deuce');
+var ace = document.getElementById('ace');
+let recipient = document.getElementById('receiver');
+let appointer = document.getElementById('giver');
+
+function cactCert(recipient, appointer){
+    if (cactuar) {
+        
+    }
+}
